971f3bebb7bccb120a4526169af1b2ceefee08e9
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-08-17 10:48:17 +0300

    Update index.py

--- index.py
+++ index.py
@@ -14,3 +14,34 @@ drop(['doc_type', 'doi', 'id', 'issue', 'lang', 'n_citation',
  'page_end', 'page_start', 'publisher', 'references', 'url', 'venue', 'volume'],
  axis=1)
  model_df.shape
+
+
+ #Collaborative filtering stage 1: Build item feature matrix
+ unique_fos = sorted(list({feature
+ for paper_row in model_df.fos.fillna('0')
+ for feature in paper_row }))
+ unique_year = sorted(model_df['year'].astype('str').unique())
+ def feature_array(x, var, unique_array):
+      row_dict = {}
+ for i in x.index:
+     var_dict = {}
+ for j in range(len(unique_array)):
+    if type(x[i]) is list:
+        if unique_array[j] in x[i]:
+           var_dict.update({var + '_' + unique_array[j]: 1})
+             else:
+              var_dict.update({var + '_' + unique_array[j]: 0})
+             else:
+           if unique_array[j] == str(x[i]):
+                var_dict.update({var + '_' + unique_array[j]: 1})
+                     else:
+                         var_dict.update({var + '_' + unique_array[j]: 0})
+                 row_dict.update({i : var_dict})
+ feature_df = pd.DataFrame.from_dict(row_dict, dtype='str').T
+ return feature_df
+# Back to the Feature: Building an Academic Paper Recommender
+ year_features = feature_array(model_df['year'], unique_year)
+ fos_features = feature_array(model_df['fos'], unique_fos)
+ first_features = fos_features.join(year_features).T
+ from sys import getsizeof
+ print('Size of first feature array: ', getsizeof(first_features))
